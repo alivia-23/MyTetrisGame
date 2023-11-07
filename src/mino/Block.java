d0a113a733093aa2830881a99e82dbf34b11fd50
alivia-23 <guin.al@husky.neu.edu>
2023-11-06 22:27:27 -0800

    create the Block class

--- src/mino/Block.java
+++ src/mino/Block.java
@@ -1,4 +1,21 @@
 package mino;
 
-public class Block {
+import java.awt.*;
+
+/**
+ * This class represents the blocks in tetris
+ */
+public class Block extends Rectangle {
+    public int x, y;
+    public static final int SIZE = 30; // 30 * 30 block
+    public Color c;
+
+    public Block(Color c) {
+        this.c = c;
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.setColor(c);
+        g2.fillRect(x, y, SIZE, SIZE);
+    }
 }
