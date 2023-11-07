827d4d47c95d9e5608073beb7021120d2a41a02f
alivia-23 <guin.al@husky.neu.edu>
2023-11-06 22:47:14 -0800

    create 7 types of tetrominos

--- src/mino/Mino.java
+++ src/mino/Mino.java
@@ -1,4 +1,26 @@
 package mino;
 
+import java.awt.*;
+
 public class Mino {
+    public Block block[] = new Block[4];
+    public Block tempBlock[] = new Block[4];
+
+    public void create(Color c) {
+        block[0] = new Block(c);
+        block[1] = new Block(c);
+        block[2] = new Block(c);
+        block[3] = new Block(c);
+        tempBlock[0] = new Block(c);
+        tempBlock[1] = new Block(c);
+        tempBlock[2] = new Block(c);
+        tempBlock[3] = new Block(c);
+    }
+
+    public void setXY(int x, int y) {}
+    public void updateXY(int direction) {}
+    public void update() {}
+    public void draw(Graphics2D g2) {
+
+    }
 }
